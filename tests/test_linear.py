2e5b44d5ae2900975f59a5b75485292fd75e45b8
jmc0 <fcnclk@outlook.com>
2021-12-29 17:06:50 -0800

    add linear cypher

---
+++ tests/test_linear.py
@@ -0,0 +1,16 @@
+import pytest
+import string
+import random
+import math
+from linear_cipher import linear_encode,linear_decode
+num_tests=1000
+
+def test_linear():
+    letters = string.ascii_letters
+    a_param = [x for x in range(26) if(math.gcd(x,26)==1)]
+    for i in range(num_tests):
+        str_len=random.randint(1,15)
+        test_str=''.join(random.choice(letters) for i in range(str_len)) 
+        test_a=random.choice(a_param)
+        test_b=random.randint(0,25)
+        assert test_str == linear_decode(linear_encode(test_str,test_a,test_b),test_a,test_b)
